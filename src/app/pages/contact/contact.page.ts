fcf8e395173b0b07a17abde1e6b50636cbca1652
Hong Wang <hongw@mun.ca>
2023-07-19 04:22:43 -0230

    CSS update

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {Contact} from "../Entity";
+import { Contact } from "../Entity";
 
 @Component({
   selector: 'app-contact',
@@ -10,7 +10,7 @@ export class ContactPage implements OnInit, OnDestroy {
   contactList: Contact[] = [{id: 33, name: 'Sears', number: 7097692008, date: new Date()},
     {id: 99, name: 'Matt', number: 7097692008, date: new Date('1991-12-17')},
     {id: 88, name: 'Brett', number: 7097692008, date: new Date('1998-12-17')},
-    {id: 88, name: 'Sanyu', number: 7097692008, date: new Date('1999-12-17')}];
+    {id: 8989, name: 'Sanyu', number: 7097692008, date: new Date('1999-12-17')}];
 
   constructor(
   ) {
@@ -24,8 +24,7 @@ export class ContactPage implements OnInit, OnDestroy {
   }
 
   addLine() {
-    this.contactList.push({id: Math.random()*100, number: null, name: null, date: new Date()})
-    console.log(this.contactList)
+    this.contactList.push({id: Math.random()*100 + Math.random()*100, number: null, name: null, date: new Date()})
   }
 
   ngOnDestroy() {
